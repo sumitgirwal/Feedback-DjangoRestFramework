2e39921ffeb00166f5c73d0c1d0af218b2058eef
nikhilvermag <sumitgirwals@gmail.com>
2023-02-26 15:49:33 +0530

    Updated design and thanks text on home page

--- FeedbackDRF/feedbackapp/models.py
+++ FeedbackDRF/feedbackapp/models.py
@@ -5,5 +5,6 @@ class Feedback(models.Model):
 	email = models.CharField(max_length=255)
 	message = models.TextField()
 	created_at = models.DateTimeField(auto_now_add=True)
+	
 	def __str__(self):
 		return f"{self.email} | {self.message}"
